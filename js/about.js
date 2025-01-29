8451d04c56c199ab1678d1529ff34ad593bd9644
Yash Pathak UCI <yashpathak1446@gmail.com>
2025-01-28 17:41:03 -0800

    Implemented tweet categorization and percentage calculation

--- js/about.js
+++ js/about.js
@@ -31,6 +31,69 @@ function parseTweets(runkeeper_tweets) {
 	// Replace the hardcoded values inside the html id and use the tolocalDateString to format it better
 	document.getElementById('firstDate').innerText = earliest_date.toLocaleDateString("en-US", options);
 	document.getElementById('lastDate').innerText = latest_date.toLocaleDateString("en-US", options);
+
+
+	// Part 1.2
+	// First create a object to map tweets with the categories:
+	const tweet_categories = {
+		completed_event: 0,
+		live_event: 0,
+		achievement: 0,
+		miscellaneous: 0,
+	};
+
+	// Debug statement to check correct indexing/output access by class tag
+	console.log(document.getElementsByClassName('liveEvents')[0].innerText);
+
+	// Categorize each tweet. For that you need to loop through the tweets:
+	tweet_array.forEach(tweet => {
+		// Check if it is a completed event
+		if (tweet.source == "completed_event") {
+			tweet_categories.completed_event++;
+		}
+		// or a live event
+		else if (tweet.source == "live_event") {
+			tweet_categories.live_event++;
+		}
+		// or an achievement
+		else if (tweet.source == "achievement") {
+			tweet_categories.achievement++;
+		}
+		// or miscellaneous
+		else if (tweet.source == "miscellaneous") {
+			tweet_categories.miscellaneous++;
+		}
+	});
+
+	// First set the value for completed
+	let completedEventsElements = document.getElementsByClassName('completedEvents');
+	for (let i = 0; i < completedEventsElements.length; i++) {
+		completedEventsElements[i].innerText = tweet_categories.completed_event;
+	}
+
+	// Next set the value for live events
+	// Use query selector if the appearance of the inner text is only once in the html
+	document.querySelector('.liveEvents').innerText = tweet_categories.live_event;
+
+	// Next set the value for achievements
+	document.querySelector('.achievements').innerText = tweet_categories.achievement;
+
+	// Next set the value for miscellaneous tweets
+	document.querySelector('.miscellaneous').innerText = tweet_categories.miscellaneous;
+
+	// Now calculate the percentages of the tweets out of the total tweets
+	// Calculated the total number of tweets:
+	const total_tweets = tweet_array.length;
+	// First set the value for the percentage of completed events. 
+	// Use math.format and 4 to get upto 2 decimal places when calculating the percentages
+	document.querySelector('.completedEventsPct').innerText = math.format((tweet_categories.completed_event / total_tweets * 100), 4) + "%";
+	// Next set value for percentage of live events
+	document.querySelector('.liveEventsPct').innerText = math.format((tweet_categories.live_event / total_tweets * 100), 4) + "%";
+	// Next set value for percentage of achievements
+	document.querySelector('.achievementsPct').innerText = math.format((tweet_categories.achievement / total_tweets * 100), 2) + "%";
+	// Set the value for percentage of miscellaneous
+	document.querySelector('.miscellaneousPct').innerText = math.format((tweet_categories.miscellaneous / total_tweets * 100), 2) + "%";
+
 }
 
 //Wait for the DOM to load
--- ts/tweet.ts
+++ ts/tweet.ts
@@ -1,33 +1,50 @@
 class Tweet {
-	private text:string;
-	time:Date;
+    private text: string;
+    time: Date;
 
-	constructor(tweet_text:string, tweet_time:string) {
+    constructor(tweet_text: string, tweet_time: string) {
         this.text = tweet_text;
-		this.time = new Date(tweet_time);//, "ddd MMM D HH:mm:ss Z YYYY"
-	}
+        this.time = new Date(tweet_time);//, "ddd MMM D HH:mm:ss Z YYYY"
+    }
 
-	//returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
-    get source():string {
+    //returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
+    get source(): string {
         //TODO: identify whether the source is a live event, an achievement, a completed event, or miscellaneous.
-        return "unknown";
+
+        // starts with "Just completed" OR "Just posted" -> 'completed_event' (95% +- 5%)
+        if (this.text.startsWith("Just completed") || this.text.startsWith("Just posted")) {
+            return "completed_event";
+        }
+        // starts with "Watch my" OR includes #RKLive-> 'live_event'
+        else if (this.text.startsWith("Watch my") || this.text.includes('#RKLive')) {
+            return "live_event";
+        }
+        // starts with "Achieved" OR includes Achieved OR endsWith #FitnessAlerts-> 'achievement'
+        else if (this.text.startsWith("Achieved") || this.text.includes("ACHIEVED") || this.text.endsWith("#FitnessAlerts")) {
+            return "achievement";
+        }
+        // else: 'miscellaneous'
+        else {
+            return "miscellaneous";
+        }
+
     }
 
     //returns a boolean, whether the text includes any content written by the person tweeting.
-    get written():boolean {
+    get written(): boolean {
         //TODO: identify whether the tweet is written
         return false;
     }
 
-    get writtenText():string {
-        if(!this.written) {
+    get writtenText(): string {
+        if (!this.written) {
             return "";
         }
         //TODO: parse the written text from the tweet
         return "";
     }
 
-    get activityType():string {
+    get activityType(): string {
         if (this.source != 'completed_event') {
             return "unknown";
         }
@@ -35,15 +52,15 @@ class Tweet {
         return "";
     }
 
-    get distance():number {
-        if(this.source != 'completed_event') {
+    get distance(): number {
+        if (this.source != 'completed_event') {
             return 0;
         }
         //TODO: prase the distance from the text of the tweet
         return 0;
     }
 
-    getHTMLTableRow(rowNumber:number):string {
+    getHTMLTableRow(rowNumber: number): string {
         //TODO: return a table row which summarizes the tweet with a clickable link to the RunKeeper activity
         return "<tr></tr>";
     }
