d188cf9d6f9b7f05085d193a7d2ec48ebb346ab8
Yash Pathak UCI <yashpathak1446@gmail.com>
2025-01-30 14:19:39 -0800

    Calculated the activity related details and updating the textual representation.

--- js/activities.js
+++ js/activities.js
@@ -1,25 +1,163 @@
 function parseTweets(runkeeper_tweets) {
 	//Do not proceed if no tweets loaded
-	if(runkeeper_tweets === undefined) {
+	if (runkeeper_tweets === undefined) {
 		window.alert('No tweets returned');
 		return;
 	}
-	
-	tweet_array = runkeeper_tweets.map(function(tweet) {
+
+	tweet_array = runkeeper_tweets.map(function (tweet) {
 		return new Tweet(tweet.text, tweet.created_at);
 	});
+
+	// create a map to store the values of the the tweet as key
+
+
+	// Create another map to count the total number of activities
+	let activity_dict = {};
+
+	// Create a dictionary based on activity dict that keeps track 
+	// of distance covered by each activity,a s well as the day
+	let distance_dict = {};
+
+	// Create a dict based on week of the day, accounting for all the distances
+	// Covered on either a weekday or weekend 
+	let week_dict = {
+		"weekdays": 0,
+		"weekends": 0
+	};
+
+	// function to 
+	function addOrIncrement(map, key) {
+		if (map[key]) {
+			map[key]++;
+		}
+		else {
+			map[key] = 1;
+		}
+	}
+
+	// Loop through every tweet to extract relevant information
+	tweet_array.forEach(tweet => {
+		// Store the activity type for each tweet
+		let tweet_activity = tweet.activityType;
+		// Store the disstance for each tweet
+		let tweet_distance = tweet.distance;
+		// Store the day that each tweet was made
+		let tweet_day = tweet.time.toLocaleString('en-us', { weekday: 'long' });
+
+		if (tweet_day == "Sunday" || tweet_day == "Saturday") {
+			tweet_day = "weekends";
+		}
+		else {
+			tweet_day = "weekdays";
+		}
+		// Check if tweet_activity is "". If so, then it is a timed workout. Exclude it
+		if (tweet_activity != "") {
+			// Increment the count of activity per activity
+			addOrIncrement(activity_dict, tweet_activity);
+			// Check if the tweet has been recorded in distance dict already
+			if (distance_dict[tweet_activity]) {
+				distance_dict[tweet_activity] += tweet_distance;
+			}
+			else {
+				distance_dict[tweet_activity] = tweet_distance;
+			}
+			// Map the distance covered on weekends/weekdays
+			week_dict[tweet_day] += tweet_distance;
+		}
+	});
+
+	// Create a list to store the values of activity dict
+	const activity_counts = [];
+	// Create a variable to keep track of all the activity types
+	let num_activities = 0;
+
+	// Loop through the activity dict
+	for (key in activity_dict) {
+		// Increment count of total activities
+		num_activities++;
+		// Push the value of the counts of each activity
+		activity_counts.push(activity_dict[key]);
+	}
+
+	// Sort the counts of activities in descending order
+	activity_counts.sort((a, b) => b - a);
+	// Store the top 3 activities by matching the sorted values with their keys
+	let top1_activity, top2_activity, top3_activity;
+	// Loop through activity dict to find the top 3 activities
+	for (key in activity_dict) {
+		// Check if it is the top 1 activity
+		if (activity_dict[key] == activity_counts[0]) {
+			top1_activity = key;
+		}
+		// Check if it is the top 2 activity
+		else if (activity_dict[key] == activity_counts[1]) {
+			top2_activity = key;
+		}
+		// check if it is the top 3 activity
+		else if (activity_dict[key] == activity_counts[2]) {
+			top3_activity = key;
+		}
+	}
+
+	// calculate the max and min of the distance dict
+	let max_distance = Math.max(...Object.values(distance_dict));
+	let max_distance_activity;
+	let min_distance = Math.min(...Object.values(distance_dict));
+	let min_distance_activity;
+
+	// Loop through the distance dict to find the relevant keys associated to these distances
+	for (key in distance_dict) {
+		if (distance_dict[key] == max_distance) {
+			max_distance_activity = key;
+		}
+		if (distance_dict[key] == min_distance) {
+			min_distance_activity = key;
+		}
+	}
+
+	// Use week dict to find which day had more covered distance
+	max_distance_day_number = Math.max(week_dict['weekdays'], week_dict['weekends']);
+	let max_distance_day;
+	if (week_dict['weekdays'] == max_distance_day_number) {
+		max_distance_day = 'weekdays';
+	}
+	else {
+		max_distance_day = 'weekends';
+	}
+
+	// 2.1.1
+	// Display all the different types of activities
+	document.getElementById('numberActivities').innerText = num_activities;
+
+	// 2.1.2
+	// Display the top 3 activities
+	document.getElementById('firstMost').innerText = top1_activity;
+	document.getElementById('secondMost').innerText = top2_activity;
+	document.getElementById('thirdMost').innerText = top3_activity;
+
+	// 2.1.3
+	// Display the activity with most and least distance
+	document.getElementById('longestActivityType').innerText = max_distance_activity;
+	document.getElementById('shortestActivityType').innerText = min_distance_activity;
+
+	// 2.1.4
+	// Display the type of day (weekend/weekday) where more distance was covered
+	document.getElementById('weekdayOrWeekendLonger').innerText = max_distance_day;
+
+
 
 	//TODO: create a new array or manipulate tweet_array to create a graph of the number of tweets containing each type of activity.
 
 	activity_vis_spec = {
-	  "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
-	  "description": "A graph of the number of Tweets containing each type of activity.",
-	  "data": {
-	    "values": tweet_array
-	  }
-	  //TODO: Add mark and encoding
+		"$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+		"description": "A graph of the number of Tweets containing each type of activity.",
+		"data": {
+			"values": tweet_array
+		}
+		//TODO: Add mark and encoding
 	};
-	vegaEmbed('#activityVis', activity_vis_spec, {actions:false});
+	vegaEmbed('#activityVis', activity_vis_spec, { actions: false });
 
 	//TODO: create the visualizations which group the three most-tweeted activities by the day of the week.
 	//Use those visualizations to answer the questions about which activities tended to be longest and when.
--- ts/tweet.ts
+++ ts/tweet.ts
@@ -7,7 +7,7 @@ class Tweet {
         this.time = new Date(tweet_time);//, "ddd MMM D HH:mm:ss Z YYYY"
     }
 
-    // returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
+    //returns either 'live_event', 'achievement', 'completed_event', or 'miscellaneous'
     get source(): string {
         // starts with "Just completed" OR "Just posted" -> 'completed_event' (95% +- 5%)
         if (this.text.startsWith("Just completed") || this.text.startsWith("Just posted")) {
@@ -28,7 +28,7 @@ class Tweet {
 
     }
 
-    // returns a boolean, whether the text includes any content written by the person tweeting.
+    //returns a boolean, whether the text includes any content written by the person tweeting.
     get written(): boolean {
         // Check if the tweet ends with the string "Check it out!"
         // If so, it is not written
@@ -53,18 +53,57 @@ class Tweet {
     }
 
     get activityType(): string {
-        if (this.source != 'completed_event') {
-            return "unknown";
+        // Only for completed tweets determine the activity type
+        // The format for completed tweets seems to be like:
+        // Just completed/posted a {distance} {unit} {activity} with @Runkeeper.
+        // Check it out! {link} #Runkeeper"
+        if (this.source == 'completed_event') {
+            // The units are either km (4301) or mi (3946) as per skimming the tweets.
+            // so the activity is right after that!
+
+            // Use .split to convert string to array and search for activity
+            // split on whitespace
+            const tweet = this.text.split(" ");
+            for (let i = 0; i < tweet.length; i++) {
+                // Check if the current index is pointing to the unit
+                // This code skips time duration activities like yoga
+                if (tweet[i] == "km" || tweet[i] == "mi") {
+                    let activity;
+                    // Check for mountain bike activities, motorsport freestyle, chair ride, nordic walk, or circuit walk
+
+                    if (tweet[i + 1] == "mtn" || tweet[i + 1] == "MySports" || tweet[i + 1] == "chair" || tweet[i + 1] == "nordic" || tweet[i + 1] == "circuit") {
+                        activity = tweet[i + 1] + " " + tweet[i + 2];
+                    }
+                    else {
+                        activity = tweet[i + 1];
+                    }
+                    return activity;
+                }
+            }
         }
-        //TODO: parse the activity type from the text of the tweet
+        // Otherwise return empty
         return "";
     }
 
     get distance(): number {
-        if (this.source != 'completed_event') {
-            return 0;
+        if (this.source == 'completed_event') {
+            const tweet = this.text.split(" ");
+            // This code skips time duration activities like yoga
+            for (let i = 0; i < tweet.length; i++) {
+                if (tweet[i] == "mi") {
+                    let distance = parseFloat(tweet[i - 1]);
+                    return distance;
+                }
+                // Check if the current index is pointing to km. 
+                // If so convert it to mi (1mi = 1.609km)
+                if (tweet[i] == "km") {
+                    let distance = parseFloat(tweet[i - 1]);
+                    let distance_miles = distance / 1.609;
+                    return distance_miles;
+                }
+            }
         }
-        //TODO: prase the distance from the text of the tweet
+        // Otherwise return 0
         return 0;
     }
 
